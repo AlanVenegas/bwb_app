17678b149cfbc5157dd099c337dea304f7e559f7
Alan Venegas <alan.noel.venegas@gmail.com>
2014-09-11 12:43:25 +0200

    Add users controller

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,15 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Welcome'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Welcome')
+    it "should have the content 'Bienvenido'" do
+      expect(page).to have_content('Bienvenido')
     end
+
+    it { should have_link("Registrate", href: signup_path) }
+
   end
 end
