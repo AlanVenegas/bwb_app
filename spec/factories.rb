ce0195e85009fc29f7fde240c7f1269a222579a5
Alan Venegas <alan.noel.venegas@gmail.com>
2014-09-12 14:14:54 +0200

    Finish delete users

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
-  factory :user do
-    sequence(:name)  { |n| "Mr #{n}" }
-    sequence(:email) { |n| "mr_#{n}@example.com"}
-    password "foobar"
-    password_confirmation "foobar"
-  end
+	factory :user do
+		sequence(:name)  { |n| "Mr #{n}" }
+		sequence(:email) { |n| "mr_#{n}@example.com"}
+		password "foobar"
+		password_confirmation "foobar"
+		factory :admin do
+			admin true
+		end
+	end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -26,21 +26,42 @@ describe "UserPages" do
         end
       end
     end
+
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end 
   end
 
-	describe "profile page" do
-		let(:user) { FactoryGirl.create(:user) }
-		before { visit user_path(user) }
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-		it { should have_content(user.name) }
-	end
+    it { should have_content(user.name) }
+  end
 
-	describe "signup page" do
-		before { visit signup_path }
-		it { should have_content('Nuevo usuario') }
-	end
+  describe "signup page" do
+    before { visit signup_path }
+    it { should have_content('Nuevo usuario') }
+  end
 
-	describe "signup" do
+  describe "signup" do
 
     before { visit signup_path }
 
