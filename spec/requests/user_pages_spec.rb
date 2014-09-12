7cfaacfbc0b2931935ae42ff5b7c4a8da7293c55
Alan Venegas <alan.noel.venegas@gmail.com>
2014-09-12 09:10:14 +0200

    Finish user edit and update

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -50,4 +50,40 @@ describe "UserPages" do
     end
   end
 
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content("Actualiza tu perfil") }
+      it { should have_title("Editar perfil") }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Guardar cambios" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Guardar cambios"
+      end
+
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Desconectar') }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end
+  end
 end
